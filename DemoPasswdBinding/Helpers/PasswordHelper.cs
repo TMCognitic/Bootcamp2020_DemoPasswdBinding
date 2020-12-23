e1b3efd8f8f807886391d5b7849d1860eb266d12
Morre Thierry <thierry.morre@cognitic.be>
2020-12-23 14:39:13 +0100

    Ajoutez des fichiers projet.

---
+++ DemoPasswdBinding/Helpers/PasswordHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DemoPasswdBinding.Helpers
+{
+    public static class PasswordHelper
+    {
+        #region IsUpdating DependencyProperty
+        private static readonly DependencyProperty IsUpdatingProperty;
+        private static bool GetIsUpdating(DependencyObject dependencyObject)
+        {
+            return (bool)dependencyObject.GetValue(IsUpdatingProperty);
+        }
+
+        private static void SetIsUpdating(DependencyObject dependencyObject, bool value)
+        {
+            dependencyObject.SetValue(IsUpdatingProperty, value);
+        }
+        #endregion
+
+        #region IsAttached DependencyProperty
+        private static readonly DependencyProperty IsAttachedProperty;
+
+        public static bool GetIsAttached(DependencyObject dependencyObject)
+        {
+            return (bool)dependencyObject.GetValue(IsAttachedProperty);
+        }
+
+        public static void SetIsAttached(DependencyObject dependencyObject, bool value)
+        {
+            dependencyObject.SetValue(IsAttachedProperty, value);
+        }
+
+        private static void OnIsAttachedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if(d is PasswordBox passwordBox)
+            {
+                if(e.OldValue is bool oldValue && oldValue && e.NewValue is bool newValue && !newValue)
+                {
+                    passwordBox.PasswordChanged -= PasswordChanged;
+                }
+
+                if (e.NewValue is bool b && b)
+                {
+                    passwordBox.PasswordChanged += PasswordChanged;
+                }
+            }
+        }
+        #endregion
+
+        #region Password DependencyProperty
+        private static readonly DependencyProperty PasswordProperty;
+        public static string GetPassword(DependencyObject dependencyObject)
+        {
+            return (string)dependencyObject.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject dependencyObject, string value)
+        {
+            dependencyObject.SetValue(PasswordProperty, value);
+        }
+
+        private static void OnPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox passwordBox)
+            {
+                if (!GetIsUpdating(passwordBox))
+                {
+                    passwordBox.PasswordChanged -= PasswordChanged;
+                    passwordBox.Password = (string)e.NewValue;
+                    passwordBox.PasswordChanged += PasswordChanged;
+                }
+            }
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                if (!GetIsUpdating(passwordBox))
+                {
+                    SetIsUpdating(passwordBox, true);
+                    SetPassword(passwordBox, passwordBox.Password);
+                    SetIsUpdating(passwordBox, false);
+                }
+            }
+        } 
+        #endregion
+
+
+        static PasswordHelper()
+        {
+            PasswordProperty = DependencyProperty.RegisterAttached("Password",
+                                                                   typeof(string),
+                                                                   typeof(PasswordHelper),
+                                                                   new FrameworkPropertyMetadata(defaultValue: string.Empty, flags: FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, propertyChangedCallback: OnPasswordChanged));
+            IsAttachedProperty = DependencyProperty.RegisterAttached("IsAttached", 
+                                                                     typeof(bool), 
+                                                                     typeof(PasswordHelper),
+                                                                     new PropertyMetadata(defaultValue:false, propertyChangedCallback:OnIsAttachedChanged));
+            IsUpdatingProperty = DependencyProperty.RegisterAttached("IsUpdating", 
+                                                                     typeof(bool), 
+                                                                     typeof(PasswordHelper), 
+                                                                     new PropertyMetadata(defaultValue:false));
+        }
+
+        
+    }
+}
---
+++ DemoPasswdBinding/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPasswdBinding.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _login;
+        private string _passwd;
+
+        public string Passwd
+        {
+            get
+            {
+                return _passwd;
+            }
+
+            set
+            {
+                if (_passwd != value)
+                {
+                    _passwd = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Passwd)));
+                }
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+
+            set
+            {
+                if (_login != value)
+                {
+                    _login = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Login)));
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            Passwd = "Test1234=";
+        }
+    }
+}
